cc949a6edc881b23b96a86d7f8543bdc2a646321
Oliver Rupprecht <oliverjrupprecht@gmail.com>
2024-10-26 17:13:11 +0100

    decluttering

--- IntelliJ Projects/Java Programs/Triangle/src/Triangle.java
+++
@@ -1,9 +0,0 @@
-public class Triangle {
-    public static void main(String[] args) {
-        triangleBuilder();
-    }
-
-    public static void triangleBuilder() {
-        System.out.println("      T\n     TTT\n    TTTTT\n   TTTTTTT\n  TTTTTTTTT\n TTTTTTTTTTT");
-    }
-}
